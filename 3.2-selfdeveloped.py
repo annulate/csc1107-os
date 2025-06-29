76092f284ce2356e656f49bded3e12cae8945923
zerokronos <travisneo8212@gmail.com>
2025-06-29 22:22:05 +0800

    Update 3.2-selfdeveloped.py

--- 3.2-selfdeveloped.py
+++ 3.2-selfdeveloped.py
@@ -1,13 +1,24 @@
 #Function to add numbers into a list separated by spacebar
 def add_to_list():
-    input_num_str = input("Enter numbers separated by spaces: ")
-    num_str = input_num_str.split()
-    numlist = []
-    for s in num_str:
-        numlist.append(int(s))
-    print("Your list is: ", numlist)
-    return numlist
-
+    while True:
+        try:
+            input_num_str = input("Enter numbers separated by spaces: ")
+            # Split the input string by spaces
+            num_str = input_num_str.split()
+            
+            # Convert each item to an integer, and handle errors
+            numlist = []
+            for s in num_str:
+                numlist.append(int(s))  # Convert to integer
+            print("Your list is: ", numlist)
+            return numlist
+        
+        except ValueError:
+            print("Invalid input. Please enter numbers only.")
+        
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            
 #Function to find largest number
 def largest(numlist):
     
